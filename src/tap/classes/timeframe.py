770b226bf14d0ea983883177e4bccbf8ec750f4a
KonanAI <konan@konanai.com>
2023-07-13 20:20:31 -0400

    Add first module - Time
    Signed-off-by: KonanAI <konan@konanai.com>

---
+++ src/tap/classes/timeframe.py
@@ -0,0 +1,159 @@
+
+from typing import Union
+
+
+class Time:
+    """Represent time in seconds or frames."""
+
+    FRAMES_PER_SECOND: int = 100
+    UNIT_TYPE: dict[str, type] = {"second": float, "frame": int}
+
+    def __init__(self, data: Union[int, float], unit: str = "second") -> None:
+        """
+        Initialize a Time object with a time value and unit.
+
+        :param Union[int, float] data: Time value in either seconds (float) or frames (int).
+        :param str unit: Unit of the time value ('second' or 'frame'), default is 'second'.
+        :raise ValueError: If an invalid unit is provided.
+        :raise TypeError: If an incorrect type for the data is provided.
+        """
+        self._validate_input(data, unit)
+
+        if unit == "second":
+            self._second = data
+            self._frame = self._convert_seconds_to_frames(data)
+        else: # unit == "frame"
+            self._frame = data
+            self._second = self._convert_frames_to_seconds(data)
+
+    def __add__(self, other: "Time") -> "Time":
+        """
+        Return the result of adding another Time object to this one.
+
+        :param Time other: The other Time object.
+        :return: The sum of this Time object and the other one.
+        :rtype: Time
+        """
+        return Time(self._second + other._second, "second")
+
+    def __sub__(self, other: "Time") -> "Time":
+        """Return the result of subtracting another Time object from this one."""
+        return Time(self._second - other._second, "second")
+
+    def __mul__(self, scalar: Union[int, float]) -> "Time":
+        """Return the result of multiplying the time by a scalar."""
+        frames = self._frame * scalar
+        if not frames.is_integer():
+            raise ValueError("Resulting frames must be an integer.")
+        return Time(int(frames), "frame")
+
+    def __truediv__(self, scalar: Union[int, float]) -> "Time":
+        """Return the result of dividing the time by a scalar."""
+        if scalar == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        frames = self._frame / scalar
+        if not frames.is_integer():
+            raise ValueError("Resulting frames must be an integer.")
+        return Time(int(frames), "frame")
+
+    def __lt__(self, other: "Time") -> bool:
+        """Return True if this time is less than the other time."""
+        return self._second < other._second
+
+    def __le__(self, other: "Time") -> bool:
+        """Return True if this time is less than or equal to the other time."""
+        return self._second <= other._second
+
+    def __gt__(self, other: "Time") -> bool:
+        """Return True if this time is greater than the other time."""
+        return self._second > other._second
+
+    def __ge__(self, other: "Time") -> bool:
+        """Return True if this time is greater than or equal to the other time."""
+        return self._second >= other._second
+
+    def __eq__(self, other: "Time") -> bool:
+        """Return True if this time is equal to the other time."""
+        return self._second == other._second
+
+    def __ne__(self, other: "Time") -> bool:
+        """Return True if this time is not equal to the other time."""
+        return self._second != other._second
+
+    @property
+    def second(self) -> float:
+        """
+        Get the time value in seconds.
+
+        :return: The time value in seconds.
+        :rtype: float
+        """
+        return self._second
+
+    @property
+    def frame(self) -> int:
+        """
+        Get the time value in frames.
+
+        :return: The time value in frames.
+        :rtype: int
+        """
+        return self._frame
+
+    def _validate_input(self, data: Union[int, float], unit: str) -> None:
+        """
+        Validate the input data and unit.
+
+        :param Union[int, float] data: Time value.
+        :param str unit: Unit of the time value.
+        :raise ValueError: If an invalid unit is provided.
+        :raise TypeError: If an incorrect type for the data is provided.
+        """
+        if unit not in self.UNIT_TYPE:
+            raise ValueError(
+                "Invalid unit. Use 'second' for seconds or 'frame' for frames."
+            )
+        if not isinstance(data, self.UNIT_TYPE[unit]):
+            raise TypeError(
+                f"Expected a {self.UNIT_TYPE[unit].__name__} for {unit}, got {type(data).__name__}."
+            )
+
+    @classmethod
+    def _convert_seconds_to_frames(cls, second: float) -> int:
+        """
+        Convert seconds to frames.
+
+        :param float second: Time value in seconds.
+        :return: Time value in frames.
+        :rtype: int
+        """
+        return round(second * cls.FRAMES_PER_SECOND)
+
+    @classmethod
+    def _convert_frames_to_seconds(cls, frame: int) -> float:
+        """
+        Convert frames to seconds.
+
+        :param int frame: Time value in frames.
+        :return: Time value in seconds.
+        :rtype: float
+        """
+        return frame / cls.FRAMES_PER_SECOND
+
+    def __str__(self) -> str:
+        """
+        Return the string representation of the Time object.
+
+        :return: String representation of the Time object.
+        :rtype: str
+        """
+        return f"Time(second={self._second}, frame={self._frame})"
+
+    def __repr__(self) -> str:
+        """
+        Return the string representation of the Time object.
+
+        :return: String representation of the Time object.
+        :rtype: str
+        """
+        return self.__str__()
---
+++ tests/tap/classes/test_timeframe.py
@@ -0,0 +1,247 @@
+
+import pytest
+from tap.classes.timeframe import Time
+
+
+def test_time_init_with_seconds():
+    # Arrange
+    data = 1.0
+    unit = "second"
+
+    # Act
+    time_obj = Time(data, unit)
+
+    # Assert
+    assert time_obj.second == data
+    assert time_obj.frame == data * Time.FRAMES_PER_SECOND
+
+
+def test_time_init_with_frames():
+    # Arrange
+    data = 100
+    unit = "frame"
+
+    # Act
+    time_obj = Time(data, unit)
+
+    # Assert
+    assert time_obj.second == data / Time.FRAMES_PER_SECOND
+    assert time_obj.frame == data
+
+
+def test_time_init_with_invalid_unit():
+    # Arrange
+    data = 1.0
+    unit = "invalid_unit"
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        Time(data, unit)
+
+
+def test_time_init_with_incorrect_data_type():
+    # Arrange
+    data = "invalid_data"
+    unit = "second"
+
+    # Act & Assert
+    with pytest.raises(TypeError):
+        Time(data, unit)
+
+
+def test_addition_of_two_times():
+    # Arrange
+    time_obj1 = Time(1.0, "second")
+    time_obj2 = Time(1.0, "second")
+
+    # Act
+    result = time_obj1 + time_obj2
+
+    # Assert
+    assert result.second == 2.0
+
+
+def test_subtraction_of_two_times():
+    # Arrange
+    time_obj1 = Time(2.0, "second")
+    time_obj2 = Time(1.0, "second")
+
+    # Act
+    result = time_obj1 - time_obj2
+
+    # Assert
+    assert result.second == 1.0
+
+
+def test_multiplication_with_scalar():
+    # Arrange
+    time_obj = Time(2.0, "second")
+    scalar = 2.0
+
+    # Act
+    result = time_obj * scalar
+
+    # Assert
+    assert result.second == 4.0
+
+
+def test_multiplication_with_non_integer_result():
+    # Arrange
+    time_obj = Time(3.11, "second")
+    scalar = 0.5
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        time_obj * scalar
+
+
+def test_division_by_scalar():
+    # Arrange
+    time_obj = Time(2.0, "second")
+    scalar = 2.0
+
+    # Act
+    result = time_obj / scalar
+
+    # Assert
+    assert result.second == 1.0
+
+
+def test_division_by_zero():
+    # Arrange
+    time_obj = Time(2.0, "second")
+    scalar = 0.0
+
+    # Act & Assert
+    with pytest.raises(ZeroDivisionError):
+        time_obj / scalar
+
+
+def test_division_with_non_integer_result():
+    # Arrange
+    time_obj = Time(2.0, "second")
+    scalar = 1.5
+
+    # Act & Assert
+    with pytest.raises(ValueError):
+        time_obj / scalar
+
+
+def test_less_than_operator():
+    # Arrange
+    time_obj1 = Time(1.0, "second")
+    time_obj2 = Time(2.0, "second")
+
+    # Act
+    result = time_obj1 < time_obj2
+
+    # Assert
+    assert result
+
+
+def test_less_than_or_equal_operator():
+    # Arrange
+    time_obj1 = Time(1.0, "second")
+    time_obj2 = Time(1.0, "second")
+
+    # Act
+    result = time_obj1 <= time_obj2
+
+    # Assert
+    assert result
+
+
+def test_greater_than_operator():
+    # Arrange
+    time_obj1 = Time(2.0, "second")
+    time_obj2 = Time(1.0, "second")
+
+    # Act
+    result = time_obj1 > time_obj2
+
+    # Assert
+    assert result
+
+
+def test_greater_than_or_equal_operator():
+    # Arrange
+    time_obj1 = Time(2.0, "second")
+    time_obj2 = Time(2.0, "second")
+
+    # Act
+    result = time_obj1 >= time_obj2
+
+    # Assert
+    assert result
+
+
+def test_equality_operator():
+    # Arrange
+    time_obj1 = Time(2.0, "second")
+    time_obj2 = Time(2.0, "second")
+
+    # Act
+    result = time_obj1 == time_obj2
+
+    # Assert
+    assert result
+
+
+def test_inequality_operator():
+    # Arrange
+    time_obj1 = Time(1.0, "second")
+    time_obj2 = Time(2.0, "second")
+
+    # Act
+    result = time_obj1 != time_obj2
+
+    # Assert
+    assert result
+
+
+def test_second_property():
+    # Arrange
+    data = 1.0
+    unit = "second"
+    time_obj = Time(data, unit)
+
+    # Act
+    result = time_obj.second
+
+    # Assert
+    assert result == data
+
+
+def test_frame_property():
+    # Arrange
+    data = 100
+    unit = "frame"
+    time_obj = Time(data, unit)
+
+    # Act
+    result = time_obj.frame
+
+    # Assert
+    assert result == data
+
+
+def test_str_representation():
+    # Arrange
+    time_obj = Time(1.0, "second")
+
+    # Act
+    result = str(time_obj)
+
+    # Assert
+    assert result == "Time(second=1.0, frame=100)"
+
+
+def test_repr_representation():
+    # Arrange
+    time_obj = Time(1.0, "second")
+
+    # Act
+    result = repr(time_obj)
+
+    # Assert
+    assert result == "Time(second=1.0, frame=100)"
