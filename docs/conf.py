5cfa737470f733117ff645e3e3a3a16739bfa045
KonanAI <110348966+konan-ai@users.noreply.github.com>
2023-07-25 08:08:31 -0400

    Update conf.py
    Signed-off-by: KonanAI <110348966+konan-ai@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,109 +1,145 @@
-# All the existing imports here
-import os
-import subprocess
-import packaging.version
-from pallets_sphinx_themes import get_version
-from pallets_sphinx_themes import ProjectLink
-from sphinx.ext.autodoc import ClassDocumenter
-from sphinx.application import Sphinx
-from sphinx.util.docstrings import prepare_docstring
+# Configuration file for the Sphinx documentation builder.
 
-# Project --------------------------------------------------------------
+# -- Project information
 
-project = "konanai"
-copyright = "2023 KonanAI LLC"
-author = "Joseph Konan"
-release, version = get_version("konanai")
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
-# General --------------------------------------------------------------
+release = '0.1'
+version = '0.1.0'
 
-master_doc = "index"
+# -- General configuration
+
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinxcontrib.log_cabinet",
-    "pallets_sphinx_themes",
-    "sphinx_issues",
-    "sphinx_tabs.tabs",
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
-autodoc_typehints = "description"
-autodoc_default_options = {
-    'member-order': 'bysource',
-    'undoc-members': True,
-}
-add_module_names = False
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-issues_github_path = "KonanAI-LLC/konanai"
+intersphinx_disabled_domains = ['std']
 
-# HTML -----------------------------------------------------------------
+templates_path = ['_templates']
 
-html_theme = "flask"
-html_theme_options = {"index_sidebar_logo": False}
-html_context = {
-    "project_links": [
-        ProjectLink("PyPI Releases", "https://pypi.org/project/konanai/"),
-        ProjectLink("Source Code", "https://github.com/KonanAI-LLC/konanai"),
-        ProjectLink("Issue Tracker", "https://github.com/KonanAI-LLC/konanai/issues/"),
-    ]
-}
-html_sidebars = {
-    "index": ["project.html", "localtoc.html", "searchbox.html"],
-    "**": ["localtoc.html", "relations.html", "searchbox.html"],
-}
-singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
-html_static_path = ["_static"]
-html_favicon = "_static/shortcut-icon.png"
-html_logo = "_static/konanai-logo.png"
-html_title = f"konanai Documentation ({version})"
-html_show_sourcelink = False
+# -- Options for HTML output
 
-exclude_patterns = ['**/konanai/src/konanai/externals/lib']
+html_theme = 'sphinx_rtd_theme'
 
-# LaTeX ----------------------------------------------------------------
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
 
-latex_documents = [(master_doc, f"konanai-{version}.tex", html_title, author, "manual")]
+# # All the existing imports here
+# import os
+# import subprocess
+# import packaging.version
+# from pallets_sphinx_themes import get_version
+# from pallets_sphinx_themes import ProjectLink
+# from sphinx.ext.autodoc import ClassDocumenter
+# from sphinx.application import Sphinx
+# from sphinx.util.docstrings import prepare_docstring
 
-# Local Extensions -----------------------------------------------------
+# # Project --------------------------------------------------------------
 
-def github_link(name, rawtext, text, lineno, inliner, options=None, content=None):
-    app = inliner.document.settings.env.app
-    release = app.config.release
-    base_url = "https://github.com/KonanAI-LLC/konanai/tree/"
+# project = "konanai"
+# copyright = "2023 KonanAI LLC"
+# author = "Joseph Konan"
+# release, version = get_version("konanai")
 
-    if text.endswith(">"):
-        words, text = text[:-1].rsplit("<", 1)
-        words = words.strip()
-    else:
-        words = None
+# # General --------------------------------------------------------------
 
-    if packaging.version.parse(release).is_devrelease:
-        url = f"{base_url}main/{text}"
-    else:
-        url = f"{base_url}{release}/{text}"
+# master_doc = "index"
+# extensions = [
+#     "sphinx.ext.autodoc",
+#     "sphinx.ext.intersphinx",
+#     "sphinxcontrib.log_cabinet",
+#     "pallets_sphinx_themes",
+#     "sphinx_issues",
+#     "sphinx_tabs.tabs",
+# ]
+# autodoc_typehints = "description"
+# autodoc_default_options = {
+#     'member-order': 'bysource',
+#     'undoc-members': True,
+# }
+# add_module_names = False
 
-    if words is None:
-        words = url
+# intersphinx_mapping = {
+#     "python": ("https://docs.python.org/3/", None),
+# }
+# issues_github_path = "KonanAI-LLC/konanai"
 
-    from docutils.nodes import reference
-    from docutils.parsers.rst.roles import set_classes
+# # HTML -----------------------------------------------------------------
 
-    options = options or {}
-    set_classes(options)
-    node = reference(rawtext, words, refuri=url, **options)
-    return [node], []
+# html_theme = "flask"
+# html_theme_options = {"index_sidebar_logo": False}
+# html_context = {
+#     "project_links": [
+#         ProjectLink("PyPI Releases", "https://pypi.org/project/konanai/"),
+#         ProjectLink("Source Code", "https://github.com/KonanAI-LLC/konanai"),
+#         ProjectLink("Issue Tracker", "https://github.com/KonanAI-LLC/konanai/issues/"),
+#     ]
+# }
+# html_sidebars = {
+#     "index": ["project.html", "localtoc.html", "searchbox.html"],
+#     "**": ["localtoc.html", "relations.html", "searchbox.html"],
+# }
+# singlehtml_sidebars = {"index": ["project.html", "localtoc.html"]}
+# html_static_path = ["_static"]
+# html_favicon = "_static/shortcut-icon.png"
+# html_logo = "_static/konanai-logo.png"
+# html_title = f"konanai Documentation ({version})"
+# html_show_sourcelink = False
 
-def setup(app: Sphinx):
-    app.add_role("gh", github_link)
+# exclude_patterns = ['**/konanai/src/konanai/externals/lib']
+
+# # LaTeX ----------------------------------------------------------------
+
+# latex_documents = [(master_doc, f"konanai-{version}.tex", html_title, author, "manual")]
+
+# # Local Extensions -----------------------------------------------------
+
+# def github_link(name, rawtext, text, lineno, inliner, options=None, content=None):
+#     app = inliner.document.settings.env.app
+#     release = app.config.release
+#     base_url = "https://github.com/KonanAI-LLC/konanai/tree/"
+
+#     if text.endswith(">"):
+#         words, text = text[:-1].rsplit("<", 1)
+#         words = words.strip()
+#     else:
+#         words = None
+
+#     if packaging.version.parse(release).is_devrelease:
+#         url = f"{base_url}main/{text}"
+#     else:
+#         url = f"{base_url}{release}/{text}"
+
+#     if words is None:
+#         words = url
+
+#     from docutils.nodes import reference
+#     from docutils.parsers.rst.roles import set_classes
+
+#     options = options or {}
+#     set_classes(options)
+#     node = reference(rawtext, words, refuri=url, **options)
+#     return [node], []
+
+# def setup(app: Sphinx):
+#     app.add_role("gh", github_link)
     
-    app.connect('build-finished', run_postprocess)
+#     app.connect('build-finished', run_postprocess)
 
-def run_postprocess(app, exception):
-    # The script and html directory paths
-    script_path = os.path.join(os.path.dirname(__file__), "postprocess.py")
-    html_dir_path = os.path.join(app.outdir)
+# def run_postprocess(app, exception):
+#     # The script and html directory paths
+#     script_path = os.path.join(os.path.dirname(__file__), "postprocess.py")
+#     html_dir_path = os.path.join(app.outdir)
     
-    # Call the postprocess script on the html directory
-    subprocess.check_call([sys.executable, script_path, html_dir_path])
+#     # Call the postprocess script on the html directory
+#     subprocess.check_call([sys.executable, script_path, html_dir_path])
