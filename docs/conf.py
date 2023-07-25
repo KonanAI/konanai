801c134a1cbf5ee7c57ae1e7143f95260b0fb5df
KonanAI <110348966+konan-ai@users.noreply.github.com>
2023-07-25 09:34:05 -0400

    Update conf.py
    Signed-off-by: KonanAI <110348966+konan-ai@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -11,21 +11,21 @@
 
 # -- General configuration
 
-# extensions = [
-#     'sphinx.ext.duration',
-#     'sphinx.ext.doctest',
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.autosummary',
-#     'sphinx.ext.intersphinx',
-# ]
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
-# intersphinx_mapping = {
-#     'python': ('https://docs.python.org/3/', None),
-#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-# }
-# intersphinx_disabled_domains = ['std']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # -- Options for HTML output
 
@@ -53,26 +53,26 @@ release, version = get_version("konanai")
 
 # # General --------------------------------------------------------------
 
-master_doc = "index"
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinxcontrib.log_cabinet",
-    "pallets_sphinx_themes",
-    "sphinx_issues",
-    "sphinx_tabs.tabs",
-]
-autodoc_typehints = "description"
-autodoc_default_options = {
-    'member-order': 'bysource',
-    'undoc-members': True,
-}
-add_module_names = False
+# master_doc = "index"
+# extensions = [
+#     "sphinx.ext.autodoc",
+#     "sphinx.ext.intersphinx",
+#     "sphinxcontrib.log_cabinet",
+#     "pallets_sphinx_themes",
+#     "sphinx_issues",
+#     "sphinx_tabs.tabs",
+# ]
+# autodoc_typehints = "description"
+# autodoc_default_options = {
+#     'member-order': 'bysource',
+#     'undoc-members': True,
+# }
+# add_module_names = False
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-}
-issues_github_path = "KonanAI-LLC/konanai"
+# intersphinx_mapping = {
+#     "python": ("https://docs.python.org/3/", None),
+# }
+# issues_github_path = "KonanAI-LLC/konanai"
 
 # # HTML -----------------------------------------------------------------
 
@@ -96,40 +96,40 @@ issues_github_path = "KonanAI-LLC/konanai"
 # html_title = f"konanai Documentation ({version})"
 # html_show_sourcelink = False
 
-# exclude_patterns = ['**/konanai/src/konanai/externals/lib']
+exclude_patterns = ['**/konanai/src/konanai/externals/lib']
 
-# # LaTeX ----------------------------------------------------------------
+# LaTeX ----------------------------------------------------------------
 
-# latex_documents = [(master_doc, f"konanai-{version}.tex", html_title, author, "manual")]
+latex_documents = [(master_doc, f"konanai-{version}.tex", html_title, author, "manual")]
 
-# # Local Extensions -----------------------------------------------------
+# Local Extensions -----------------------------------------------------
 
-# def github_link(name, rawtext, text, lineno, inliner, options=None, content=None):
-#     app = inliner.document.settings.env.app
-#     release = app.config.release
-#     base_url = "https://github.com/KonanAI-LLC/konanai/tree/"
+def github_link(name, rawtext, text, lineno, inliner, options=None, content=None):
+    app = inliner.document.settings.env.app
+    release = app.config.release
+    base_url = "https://github.com/KonanAI-LLC/konanai/tree/"
 
-#     if text.endswith(">"):
-#         words, text = text[:-1].rsplit("<", 1)
-#         words = words.strip()
-#     else:
-#         words = None
+    if text.endswith(">"):
+        words, text = text[:-1].rsplit("<", 1)
+        words = words.strip()
+    else:
+        words = None
 
-#     if packaging.version.parse(release).is_devrelease:
-#         url = f"{base_url}main/{text}"
-#     else:
-#         url = f"{base_url}{release}/{text}"
+    if packaging.version.parse(release).is_devrelease:
+        url = f"{base_url}main/{text}"
+    else:
+        url = f"{base_url}{release}/{text}"
 
-#     if words is None:
-#         words = url
+    if words is None:
+        words = url
 
-#     from docutils.nodes import reference
-#     from docutils.parsers.rst.roles import set_classes
+    from docutils.nodes import reference
+    from docutils.parsers.rst.roles import set_classes
 
-#     options = options or {}
-#     set_classes(options)
-#     node = reference(rawtext, words, refuri=url, **options)
-#     return [node], []
+    options = options or {}
+    set_classes(options)
+    node = reference(rawtext, words, refuri=url, **options)
+    return [node], []
 
 # def setup(app: Sphinx):
 #     app.add_role("gh", github_link)
